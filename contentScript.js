09f92bc3471b45dc7b6f1e52c7cd154ee0a108f6
Prateek Surana <prateeksurana3255@gmail.com>
2020-02-11 21:14:18 +0530

    :sparkles: Introduced message passing

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,18 @@
-console.log('Content script is running...');
+function replaceText(){
+    const FIND = "the";
+    const REPLACE_WITH = "replacely";
 
-const FIND = "the";
-const REPLACE_WITH = "replacely";
+    const regex = new RegExp(FIND, "gi");
 
-const regex = new RegExp(FIND, "gi");
+    const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
 
-const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
-
-for(const snippet of contents){
-    snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
+    for(const snippet of contents){
+        snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
+    }
 }
+
+chrome.runtime.onMessage.addListener(function(message){
+    if(message.action === 'REPLACE_TEXT'){
+        replaceText();
+    }
+});
